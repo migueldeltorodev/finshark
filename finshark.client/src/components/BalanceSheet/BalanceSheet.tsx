46444e24bb895051c4fee2c4f9b849e0bdd30929
Miguel David González del Toro <migueldgdt@estudiantes.uci.cu>
2024-08-28 13:44:31 -0400

    adding a balance sheet

---
+++ finshark.client/src/components/BalanceSheet/BalanceSheet.tsx
@@ -0,0 +1,87 @@
+import { useOutletContext } from "react-router";
+import { CompanyBalanceSheet } from "../../company";
+import { useEffect, useState } from "react";
+import { getBalanceSheet } from "../../api";
+import RatioList from "../RatioList/RatioList";
+
+type Props = {};
+
+const config = [
+  {
+    label: <div className="font-bold">Total Assets</div>,
+    render: (company: CompanyBalanceSheet) => company.totalAssets,
+  },
+  {
+    label: "Current Assets",
+    render: (company: CompanyBalanceSheet) => company.totalCurrentAssets,
+  },
+  {
+    label: "Total Cash",
+    render: (company: CompanyBalanceSheet) => company.cashAndCashEquivalents,
+  },
+  {
+    label: "Property & equipment",
+    render: (company: CompanyBalanceSheet) => company.propertyPlantEquipmentNet,
+  },
+  {
+    label: "Intangible Assets",
+    render: (company: CompanyBalanceSheet) => company.intangibleAssets,
+  },
+  {
+    label: "Long Term Debt",
+    render: (company: CompanyBalanceSheet) => company.longTermDebt,
+  },
+  {
+    label: "Total Debt",
+    render: (company: CompanyBalanceSheet) => company.otherCurrentLiabilities,
+  },
+  {
+    label: <div className="font-bold">Total Liabilites</div>,
+    render: (company: CompanyBalanceSheet) => company.totalLiabilities,
+  },
+  {
+    label: "Current Liabilities",
+    render: (company: CompanyBalanceSheet) => company.totalCurrentLiabilities,
+  },
+  {
+    label: "Long-Term Debt",
+    render: (company: CompanyBalanceSheet) => company.longTermDebt,
+  },
+  {
+    label: "Long-Term Income Taxes",
+    render: (company: CompanyBalanceSheet) => company.otherLiabilities,
+  },
+  {
+    label: "Stakeholder's Equity",
+    render: (company: CompanyBalanceSheet) => company.totalStockholdersEquity,
+  },
+  {
+    label: "Retained Earnings",
+    render: (company: CompanyBalanceSheet) => company.retainedEarnings,
+  },
+];
+
+function BalanceSheet({}: Props) {
+  const ticker = useOutletContext<string>();
+  const [balanceSheet, setBalanceSheet] = useState<CompanyBalanceSheet>();
+
+  useEffect(() => {
+    const getBalanceData = async () => {
+      const value = await getBalanceSheet(ticker!);
+      setBalanceSheet(value?.data[0]);
+    };
+    getBalanceData();
+  }, []);
+
+  return (
+    <>
+      {balanceSheet ? (
+        <RatioList config={config} data={balanceSheet} />
+      ) : (
+        <h1>Company not Found!</h1>
+      )}
+    </>
+  );
+}
+
+export default BalanceSheet;
--- finshark.client/src/routes/Routes.tsx
+++ finshark.client/src/routes/Routes.tsx
@@ -6,6 +6,7 @@ import SearchPage from "../pages/SearchPage/SearchPage";
 import CompanyProfile from "../components/CompanyProfile/CompanyProfile";
 import IncomeStatement from "../components/IncomeStatement/IncomeStatement";
 import DesignGuide from "../pages/DesignGuide/DesignGuide";
+import BalanceSheet from "../components/BalanceSheet/BalanceSheet";
 
 export const router = createBrowserRouter([
   {
@@ -21,6 +22,7 @@ export const router = createBrowserRouter([
         children: [
           { path: "company-profile", element: <CompanyProfile /> },
           { path: "income-statement", element: <IncomeStatement /> },
+          { path: "balance-sheet", element: <BalanceSheet /> },
         ],
       },
     ],
