7fd97e5840fba434880a3f0c474310672a53fe5b
Miguel David González del Toro <migueldgdt@estudiantes.uci.cu>
2024-09-13 09:38:40 -0400

    refactor(navbar): added a logout system

--- finshark.client/src/components/Navbar/Navbar.tsx
+++ finshark.client/src/components/Navbar/Navbar.tsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
 import logoHomePage from "./logo.png";
 import "./Navbar.css";
+import { useAuth } from "../../context/useAuth";
 
 interface Props {}
 
 const Navbar = ({}: Props) => {
+  const { isLoggedIn, user, logout } = useAuth();
   return (
     <nav className="relative container mx-auto p-6">
       <div className="flex items-center justify-between">
@@ -18,17 +20,29 @@ const Navbar = ({}: Props) => {
             </Link>
           </div>
         </div>
-        <div className="hidden lg:flex items-center space-x-6 text-back">
-          <a href="login" className="hover:text-darkBlue">
-            Login
-          </a>
-          <a
-            href="register"
-            className="px-8 py-3 font-bold rounded text-white bg-lightGreen hover:opacity-70"
-          >
-            Signup
-          </a>
-        </div>
+        {isLoggedIn() ? (
+          <div className="hidden lg:flex items-center space-x-6 text-back">
+            <div>Welcome, {user?.userName}</div>
+            <a
+              onClick={logout}
+              className="px-8 py-3 font-bold rounded text-white bg-lightGreen hover:opacity-70"
+            >
+              Logout
+            </a>
+          </div>
+        ) : (
+          <div className="hidden lg:flex items-center space-x-6 text-back">
+            <Link to="/login" className="font-bold hover:text-darkBlue">
+              Login
+            </Link>
+            <Link
+              to="/register"
+              className="px-8 py-3 font-bold rounded text-white bg-lightGreen hover:opacity-70"
+            >
+              Signup
+            </Link>
+          </div>
+        )}
       </div>
     </nav>
   );
